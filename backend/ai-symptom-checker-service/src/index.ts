77782ba1911d0eb436e54a48daabbeef862dac7f
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2026-04-06 11:09:58 +0530

    feat(ai): defined middlewares

--- backend/ai-symptom-checker-service/src/index.ts
+++ backend/ai-symptom-checker-service/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser"
 import routes from "./routes";
 import { CONFIG } from "./config/envConfig";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/", routes);
 
